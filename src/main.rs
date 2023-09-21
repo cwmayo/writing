cbfd674c54abbf478130d5545c55ec5a264a374b
Colten Kiehne <cokiehne@gmail.com>
2023-09-21 10:41:06 -0500

    Update index.html

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,25 @@
 use std::fs;
 use std::fs::File;
 use std::io::Write;
+
+
 fn main() -> std::io::Result<()> {
-    println!("Hello, world!");
-    let mut file = File::create("./output/test.txt")?;
-    file.write_all(b"Hello?")?;
+    println!("Starting build process");
+
+    // remove placeholder file
+    fs::remove_file("./output/delete.md")?;
+
+    let mut file = create_file("cool.txt").unwrap();
+
+
+
     
+
     Ok(())
 }
+
+fn create_file(name: &str) -> Result<File, std::io::Error> {
+    return File::create(format!("./output/{}", name));
+}
+
+
