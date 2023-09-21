1c89d2600177868753d1eede5892c842afb4a6e1
Colten Kiehne <cokiehne@gmail.com>
2023-09-21 10:43:48 -0500

    Remove placeholder file in rs

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,6 @@ use std::io::Write;
 
 fn main() -> std::io::Result<()> {
     println!("Starting build process");
-
-    // remove placeholder file
-    fs::remove_file("./output/delete.md")?;
-
     let mut file = create_file("cool.txt").unwrap();
 
 
