efd78aa2831d4e65c8f1631bf5eba5b4c14cf8ec
Colten Kiehne <cokiehne@gmail.com>
2023-10-22 15:30:22 -0500

    Style changes

--- output/index-script.js
+++ output/index-script.js
@@ -1,19 +1,3 @@
 const stories = document.getElementById("short-story-header");
 const poems = document.getElementById("poem-header");
 const other = document.getElementById("other-header");
-
-makeClickable(stories);
-makeClickable(poems);
-makeClickable(other);
-
-function makeClickable(element) {
-    element.addEventListener('click', event => {
-        if(element.parentElement.classList.contains("hidden")) {
-            element.parentElement.classList.remove("hidden");
-            element.parentElement.classList.add("shown");
-        } else {
-            element.parentElement.classList.remove("shown");
-            element.parentElement.classList.add("hidden");
-        }
-    });
-}
